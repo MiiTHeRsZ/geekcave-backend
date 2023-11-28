249055a69d65eea7a65dfa887f483ffed71c1e75
MiiTHeRsZ <gustavo_leme05@hotmail.com>
2023-11-28 10:33:29 -0300

    Initial commit

---
+++ src/main/java/gusta/miithersz/geekcave/GeekcaveApplication.java
@@ -0,0 +1,13 @@
+package gusta.miithersz.geekcave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekcaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeekcaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gusta/miithersz/geekcave/GeekcaveApplicationTests.java
@@ -0,0 +1,13 @@
+package gusta.miithersz.geekcave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekcaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
